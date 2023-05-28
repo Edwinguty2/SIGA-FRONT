cd1da2a3a5d459763b4c27cc49cd50535702f1a3
Miguel Feo <mfemerchan@gmail.com>
2023-05-28 18:39:12 -0500

    Permitir mostrar el formulario y la respuesta

--- src/componentes/paginas/Crear.jsx
+++ src/componentes/paginas/Crear.jsx
@@ -1,9 +1,19 @@
+import React, { useState } from 'react';
+import FormularioAsignaturas from '../creacion/FormularioAsignaturas';
+
+
 export const Crear = () => {
-	return (
-		<>
-			<div>
-				<h1>Este es la pagina de crear</h1>
-			</div>
-		</>
-	)
-}
+  const [asignaturas, setAsignaturas] = useState([]);
+
+  const agregarAsignatura = (asignatura) => {
+    setAsignaturas([...asignaturas, asignatura]);
+  };
+
+  return (
+    <div>
+      <h1>Formulario de Asignaturas</h1>
+      <FormularioAsignaturas agregarAsignatura={agregarAsignatura} />
+    
+    </div>
+  );
+};
