a487af930ef3e0bfd4234d387d02ca60483a9f57
EdwinGuty <edwingutrou@unisabana.edu.co>
2023-05-28 21:30:18 -0500

    correcion

--- src/SigaApp.jsx
+++ src/SigaApp.jsx
@@ -1,10 +1,69 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Crear } from './componentes/paginas/Crear';
+import Crear from "./componentes/paginas/Crear";
 import { CursoLibre } from './componentes/paginas/CursoLibre';
 import { Navbar } from "./componentes/navegacion/Navbar";
 import { Inicio } from "./componentes/paginas/Inicio";
+import { useState } from 'react';
 
 export const SigaApp = () => {
+
+	const [facultades, setFacultades] = useState({
+		ingenieria: {
+			estudiantes: [],
+			profesores: [],
+			asignaturas: []
+		},
+		medicina: {
+			estudiantes: [],
+			profesores: [],
+			asignaturas: []
+		},
+		comunicacion: {
+			estudiantes: [],
+			profesores: [],
+			asignaturas: []
+		},
+		educacion: {
+			estudiantes: [],
+			profesores: [],
+			asignaturas: []
+		},
+		derecho: {
+			estudiantes: [],
+			profesores: [],
+			asignaturas: []
+		}
+	});
+
+
+	const agregarEstudiante = (estudiante, facultad) => {
+		setFacultades((facultades) => {
+
+			const nuevasFacultades = { ...facultades };
+
+
+			const facultadEspecifica = nuevasFacultades[facultad];
+
+			const nuevaListaEstudiantes = [...facultadEspecifica.estudiantes, estudiante];
+
+
+			facultadEspecifica.estudiantes = nuevaListaEstudiantes;
+
+			return nuevasFacultades;
+		});
+
+	}
+	// const editarEstudiante = (estudianteEditado) => {
+	// 	setEstudiantes(estudiantes.map((estudiante) => {
+	// 		if (estudianteEditado.id === estudiante.id) {
+	// 			return estudianteEditado;
+	// 		} else {
+	// 			return estudiante
+	// 		}
+	// 	}));
+
+	// }
+
 	return (
 		<>
 			<div className='SigaApp'>
@@ -12,7 +71,7 @@ export const SigaApp = () => {
 					<Navbar />
 					<Switch>
 						<Route path='/' exact component={Inicio} />
-						<Route path='/crear' component={Crear} />
+						<Route path='/crear' render={(props) => ( <Crear {...props} agregarEstudiante={(estudiante) => { agregarEstudiante(estudiante) }} /> )}/>
 						<Route path='/curso-libre' component={CursoLibre} />
 					</Switch>
 				</Router>
